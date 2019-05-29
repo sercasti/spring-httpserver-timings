91464040af3a76b0c0f6e789573e621d3460557f
sergio.castineyras <sergio.castineyras@globallogic.com>
2019-05-28 22:59:38 -0300

    public config to support extension

--- src/main/java/io/github/sercasti/tracing/config/TracingConfig.java
+++ src/main/java/io/github/sercasti/tracing/config/TracingConfig.java
@@ -16,7 +16,7 @@ public class TracingConfig {
 
     @Bean(name = "tracing")
     @RequestScope
-    protected Tracing createTracing() {
+    public static Tracing createTracing() {
         return (Tracing) Proxy.newProxyInstance(TracingConfig.class.getClassLoader(), new Class[]{Tracing.class}, new InvocationHandler() {
             @Override
             public Object invoke(final Object proxy, final Method method, final Object[] args) throws Throwable {
